97f2befb6d5a8b25135f8b2dcb53d3bdd26c96bc
한상우 <sktkddn777@naver.com>
2023-07-18 17:10:47 +0900

    [Init] BE 프로젝트 초기 세팅

---
+++ Back-End/fashionity/src/test/java/com/infinity/fashionity/FashionityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.fashionity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
