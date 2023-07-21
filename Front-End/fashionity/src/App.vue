7b76f575cad468d420afb6ade4614abda17711c9
정지원 <jiwon7164@naver.com>
2023-07-21 18:00:10 +0900

    [Init] Fe Vue 초기 세팅 - react 삭제 - vue 생성

---
+++ Front-End/fashionity/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <img alt="Test Image" src="./assets/img/testImage.jpeg"> -->
+    <h2>font test</h2>
+    <h2>한글 테스트</h2>
+
+    <TheFooter></TheFooter>
+
+    
+
+  </div>
+  
+
+</template>
+
+<script>
+import TheFooter from './components/TheFooter.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    TheFooter,
+  }
+}
+</script>
+
+<style>
+@import "./App.css";
+</style>
