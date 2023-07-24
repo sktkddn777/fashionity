5cb3aeb270f84b6b8afd3768019bb332c67acc22
배이경 <yikyung52@naver.com>
2023-07-24 17:45:18 +0900

    [Feat] created consultant, schedule entity

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/consultants/entity/ConsultantEntity.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/consultants/entity/ConsultantEntity.java
@@ -1,14 +1,33 @@
 package com.infinity.fashionity.consultants.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.infinity.fashionity.global.entity.CUDEntity;
+import com.infinity.fashionity.members.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import org.hibernate.annotations.SQLDelete;
+import javax.persistence.*;
+
 
 @Entity
 @Table(name = "consultant_infos")
-public class ConsultantEntity {
+@SQLDelete(sql = "UPDATE Consultant SET deleted_at = now() WHERE seq = ?")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ConsultantEntity extends CUDEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "consultant_info_seq")
     private Long seq;
+
+    @Column(name = "consultant_info_level", length = 20, unique = false, nullable = false)
+    private String level;
+
+    @Id
+    @JoinColumn(name = "member_seq", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    private MemberEntity seq;
 }
+
--- Back-End/fashionity/src/main/java/com/infinity/fashionity/consultants/entity/ScheduleEntity.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/consultants/entity/ScheduleEntity.java
@@ -1,14 +1,42 @@
 package com.infinity.fashionity.consultants.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.infinity.fashionity.global.entity.CUDEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "consultant_schedule")
-public class ScheduleEntity {
+@SQLDelete(sql="UPDATE Schedule SET deleted_at = now() WHERE seq = ?")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduleEntity extends CUDEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "schedule_seq")
     private Long seq;
+
+    @Id
+    @JoinColumn(name="consultant_info_seq", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name="consultant_info_seq", nullable = false)
+    private ConsultantEntity seq;
+
+    @Column(name = "availabe_datetime", unique = false, nullable = false)
+    private LocalDateTime available_datetime;
+
+    @Column(name="is_available", unique = false, nullable = false)
+    private Boolean is_available;
+
+    @Column(name="cancel_reason", length=200, unique = false, nullable = true)
+    private String cancel_reason;
+
+
+
 }
