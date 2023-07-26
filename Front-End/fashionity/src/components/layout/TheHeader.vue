ad26413c45f01c397a765f377ba42f6627c5fc29
정지원 <jiwon7164@naver.com>
2023-07-26 15:16:37 +0900

    [Style] common component layout 수정

--- Front-End/fashionity/src/components/layout/TheHeader.vue
+++ Front-End/fashionity/src/components/layout/TheHeader.vue
@@ -1,10 +1,15 @@
 <template lang="">
   <div>
     <the-nav-bar></the-nav-bar>
-
     <div class="container">
       <div class="row">
         <div class="col-1" id="logo">Fashionity</div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col col-lg-2 header-tab">HOME</div>
+        <div class="col col-lg-2 header-tab">CONSULTING</div>
+        <div class="col col-lg-2 header-tab">LIVE</div>
+        <div class="col col-lg-2 header-tab">MY PAGE</div>
       </div>
     </div>
   </div>
@@ -19,10 +24,16 @@ export default {
 };
 </script>
 <style>
+.header-tab {
+  font-style: normal;
+  font-size: 25px;
+  line-height: 38px;
+
+  text-align: center;
+}
 #logo {
   margin-left: 3%;
   font-style: normal;
-  font-weight: 600;
   font-size: 50px;
 }
 </style>
--- Front-End/fashionity/src/main.js
+++ Front-End/fashionity/src/main.js
@@ -1,8 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
