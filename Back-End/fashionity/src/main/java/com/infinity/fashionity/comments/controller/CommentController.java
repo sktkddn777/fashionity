5281fa4a27af33a38cb2c819e6c548ebabb5023b
김현욱 <rlagusdnr120@gmail.com>
2023-07-27 17:47:39 +0900

    [Feat] CommentController 완성
    - 해당 api에 맞게 controller를 작성

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/comments/controller/CommentController.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/comments/controller/CommentController.java
@@ -1,7 +1,6 @@
 package com.infinity.fashionity.comments.controller;
 
-import com.infinity.fashionity.comments.dto.CommentListDTO;
-import com.infinity.fashionity.comments.dto.CommentSaveDTO;
+import com.infinity.fashionity.comments.dto.*;
 import com.infinity.fashionity.comments.service.CommentService;
 import com.infinity.fashionity.posts.dto.PostDetailDTO;
 import com.infinity.fashionity.security.dto.JwtAuthentication;
@@ -18,33 +17,79 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 @Slf4j
 public class CommentController {
     private final CommentService commentService;
-    @GetMapping("/{postSeq}/comments")
+
+    @GetMapping(value = "/{postSeq}/comments", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<CommentListDTO.Response> getCommentList(
             @PathVariable Long postSeq, CommentListDTO.Request dto
-    ){
+    ) {
         dto.setPostSeq(postSeq);
-        CommentListDTO.Response response = CommentListDTO.Response.builder()
-                .prev(false)
-                .next(false)
-                .page(dto.getPage())
-                .comments(new ArrayList<>())
-                .build();
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(commentService.getList(dto), HttpStatus.OK);
     }
 
-    @PostMapping("/{postSeq}/comments")
+    @PostMapping(value = "/{postSeq}/comments", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<CommentSaveDTO.Response> saveComment(
             @AuthenticationPrincipal JwtAuthentication auth,
             @PathVariable Long postSeq,
-            @RequestBody CommentSaveDTO.Request dto){
+            @RequestBody CommentSaveDTO.Request dto) {
         dto.setMemberSeq(auth.getSeq());
         dto.setPostSeq(postSeq);
-        return new ResponseEntity<>(commentService.save(dto),HttpStatus.CREATED);
+        return new ResponseEntity<>(commentService.save(dto), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/{postSeq}/comments/{commentSeq}/report", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentReportDTO.Response> reportComment(
+            @AuthenticationPrincipal JwtAuthentication auth,
+            @PathVariable Long postSeq,
+            @PathVariable Long commentSeq,
+            @RequestBody CommentReportDTO.Request dto) {
+        dto.setMemberSeq(auth.getSeq());
+        dto.setPostSeq(postSeq);
+        dto.setCommentSeq(commentSeq);
+        return new ResponseEntity<>(commentService.report(dto), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{postSeq}/comments/{commentSeq}/like", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentLikeDTO.Response> likeComment(
+            @AuthenticationPrincipal JwtAuthentication auth,
+            @PathVariable Long postSeq,
+            @PathVariable Long commentSeq,
+            @RequestBody CommentLikeDTO.Request dto) {
+        dto.setCommentSeq(commentSeq);
+        dto.setPostSeq(postSeq);
+        dto.setMemberSeq(auth.getSeq());
+        return new ResponseEntity<>(commentService.like(dto), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{postSeq}/comments/{commentSeq}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentUpdateDTO.Response> updateComment(
+            @AuthenticationPrincipal JwtAuthentication auth,
+            @PathVariable Long postSeq,
+            @PathVariable Long commentSeq,
+            @RequestBody CommentUpdateDTO.Request dto) {
+        dto.setMemberSeq(auth.getSeq());
+        dto.setCommentSeq(commentSeq);
+        dto.setPostSeq(postSeq);
+        return new ResponseEntity<>(commentService.update(dto), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{postSeq}/comments/{commentSeq}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommentDeleteDTO.Response> deleteComment(
+            @AuthenticationPrincipal JwtAuthentication auth,
+            @PathVariable Long postSeq,
+            @PathVariable Long commentSeq,
+            @RequestBody CommentDeleteDTO.Request dto) {
+        dto.setCommentSeq(commentSeq);
+        dto.setPostSeq(postSeq);
+        dto.setMemberSeq(auth.getSeq());
+
+        return new ResponseEntity<>(commentService.delete(dto), HttpStatus.OK);
     }
 }
