86e2e154c4d634df5bb9ca0d1754861f8e090676
kimeunsol <kimeunsol95@naver.com>
2023-07-30 17:20:00 +0900

    [Feat] 게시판 서비스 추가

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/posts/service/PostServiceImpl.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/posts/service/PostServiceImpl.java
@@ -4,18 +4,26 @@ import com.infinity.fashionity.comments.repository.CommentRepository;
 import com.infinity.fashionity.global.exception.*;
 import com.infinity.fashionity.global.utils.StringUtils;
 import com.infinity.fashionity.members.data.MemberRole;
+import com.infinity.fashionity.members.entity.FollowEntity;
+import com.infinity.fashionity.members.entity.FollowKey;
 import com.infinity.fashionity.members.entity.MemberEntity;
+import com.infinity.fashionity.members.repository.FollowRepository;
 import com.infinity.fashionity.members.repository.MemberRepository;
 import com.infinity.fashionity.posts.dto.*;
 import com.infinity.fashionity.posts.entity.*;
 import com.infinity.fashionity.posts.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,56 +36,113 @@ public class PostServiceImpl implements PostService{
     private final HashtagRepository hashtagRepository;
     private final PostHashtagRepository postHashtagRepository;
     private final PostImageRepository postImageRepository;
-    private final CommentRepository commentRepository;
+    private final FollowRepository followRepository;
 
     // 게시글 전체 조회
     @Override
     public PostListDTO.Response getAllPosts(PostListDTO.Request dto) {
+        int page = dto.getPage();
+        int size = dto.getSize();
+        String s = dto.getS();
+        Long memberSeq = dto.getMemberSeq();
 
-        return null;
+        // s 기준으로 paging처리 (s 기본값 popular)
+        Pageable pageable = PageRequest.of(page, size, Sort.by(s).descending());
+
+        // page, size에 맞게 게시물 목록 가져오기
+        Page<PostEntity> result = postRepository.findAll(pageable);
+
+        List<PostListDTO.Post> posts = result.getContent().stream()
+                .map(entity -> {
+                    List<String> imageUrls = entity.getPostImages().stream()
+                            .map(imageEntity -> imageEntity.getUrl())
+                            .collect(Collectors.toList());
+                    // 좋아요 여부
+                    PostLikeKey likeKey = PostLikeKey.builder()
+                            .post(entity.getSeq())
+                            .member(memberSeq)
+                            .build();
+                    Optional<PostLikeEntity> postLike = postLikeRepository.findById(likeKey);
+                    boolean isLike = false;
+                    if(postLike.isPresent()){
+                        isLike = true;
+                    }
+                    return PostListDTO.Post.builder()
+                            .postSeq(entity.getSeq())
+                            .name(entity.getMember().getNickname())
+                            .profileImg(entity.getMember().getProfileUrl())
+                            .content(entity.getContent())
+                            .commentCount(entity.getCommentCount())
+                            .likeCount(entity.getLikeCount())
+                            .images(imageUrls)
+                            .liked(isLike)
+                            .build();
+                }).collect(Collectors.toList());
+        return PostListDTO.Response.builder()
+                .posts(posts)
+                .build();
     }
 
     // 게시글 상세 조회
     @Override
     @Transactional(readOnly = true)
     public PostDetailDTO.Response getPost(PostDetailDTO.Request dto) {
-        long postSeq = dto.getPostSeq();
+        Long postSeq = dto.getPostSeq();
+        Long memberSeq = dto.getMemberSeq();
 
         // 게시글 있는지 확인
         PostEntity post = postRepository.findById(postSeq)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.POST_NOT_FOUND));
 
         // 이미지
-        List<PostImageEntity> images = post.getPostImages();
-        List<String> imageUrls = new ArrayList<>();
-        for(int i = 0; i < images.size(); i++){
-            String url = images.get(i).getUrl();
-            imageUrls.add(url);
-        }
-
-        // 좋아요 수
-        long likeCount = postLikeRepository.countBy(postSeq);
-
-        // 댓글 수
-        long commentCount = commentRepository.countBy(postSeq);
+        List<String> imageUrls = post.getPostImages().stream()
+                .map(postImageEntity -> postImageEntity.getUrl())
+                .collect(Collectors.toList());
 
         // 좋아요 여부
-
+        // 게시글 좋아요 복합키 생성
+        PostLikeKey likeKey = PostLikeKey.builder()
+                .post(postSeq)
+                .member(memberSeq)
+                .build();
+        // 복합키 있는지 확인
+        Optional<PostLikeEntity> postLike = postLikeRepository.findById(likeKey);
+        boolean isLike = false;
+        if(postLike.isPresent()){
+            isLike = true;
+        }
 
         // 팔로잉 여부
+        // 팔로우 복합키 생성
+        FollowKey followKey = FollowKey.builder()
+                .followedMember(post.getMember().getSeq())
+                .member(memberSeq)
+                .build();
+        // 복합키 있는지 확인
+        Optional<FollowEntity> follow = followRepository.findById(followKey);
+        boolean isFollow = false;
+        if(follow.isPresent()){
+            isFollow = true;
+        }
 
         // 게시글 만들기
-        PostDetailDTO.Post PostDetail = PostDetailDTO.Post.builder()
+        PostDetailDTO.Post postDetail = PostDetailDTO.Post.builder()
                 .name(post.getMember().getNickname())
                 .profileImg(post.getMember().getProfileIntro())
                 .postSeq(postSeq)
                 .content(post.getContent())
                 .images(imageUrls)
-                .commentCount(commentCount)
-                .likeCount(likeCount)
+                .liked(isLike)
+                .following(isFollow)
+                .commentCount(post.getCommentCount())
+                .likeCount(post.getLikeCount())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
                 .build();
 
-        return null;
+        return PostDetailDTO.Response.builder()
+                .post(postDetail)
+                .build();
     }
 
     // 게시글 등록
@@ -105,6 +170,7 @@ public class PostServiceImpl implements PostService{
 
         // 영속화
         postRepository.save(post);
+
         // 해시태그 등록 및 영속화
         for(int i = 0; i < hashtags.size(); i++){
             HashtagEntity hashtag = HashtagEntity.builder()
@@ -132,11 +198,38 @@ public class PostServiceImpl implements PostService{
                 .build();
     }
 
-    // 게시글 수정
+    // 게시글 수정 (이미지, 해시태그 수정!!!!!)
     @Override
     @Transactional
     public PostUpdateDTO.Response updatePost(PostUpdateDTO.Request dto) {
-        return null;
+        Long postSeq = dto.getPostSeq();
+        Long memberSeq = dto.getMemberSeq();
+        String content = dto.getContent();
+
+        // 입력값 검증
+        if(memberSeq == null || postSeq == null || StringUtils.isBlank(content)){
+            throw new ValidationException(ErrorCode.MISSING_INPUT_VALUE);
+        }
+
+        // 게시글 작성한 멤버 존재하는지 확인
+        MemberEntity member = memberRepository.findById(memberSeq)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+        // 게시글 존재하는지 확인
+        PostEntity post = postRepository.findById(postSeq)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.POST_NOT_FOUND));
+
+        // 작성자와 일치하는지 확인
+        if(!post.getMember().getSeq().equals(memberSeq)){
+            throw new AccessDeniedException(ErrorCode.HANDLE_ACCESS_DENIED);
+        }
+
+        // 게시글 업데이트
+        post.updateContent(content);
+
+        return PostUpdateDTO.Response.builder()
+                .success(true)
+                .build();
     }
 
     // 게시글 삭제
@@ -160,7 +253,7 @@ public class PostServiceImpl implements PostService{
                 .orElseThrow(() -> new NotFoundException(ErrorCode.POST_NOT_FOUND));
 
         // 작성자, 권한자만 삭제 가능
-        if(memberSeq != post.getMember().getSeq()
+        if(memberSeq.equals(post.getMember().getSeq())
                 || !member.getMemberRoles().contains(MemberRole.ADMIN)){
             throw new AccessDeniedException(ErrorCode.HANDLE_ACCESS_DENIED);
         }
