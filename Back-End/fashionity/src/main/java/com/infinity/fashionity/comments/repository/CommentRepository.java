a13073911230d6da1e59cb005775b02234a83a23
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-07-31 20:43:11 +0900

    [Refactor] Comment의 정보 및 좋아요 개수, 상태 정보를 가져오는 쿼리 최적화
    entity를 가져온 뒤 연관관계 매핑 및 filter를 이용한 방법에서 query문으로 한번에 가져오도록 수정

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/comments/repository/CommentRepository.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/comments/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package com.infinity.fashionity.comments.repository;
 
 import com.infinity.fashionity.comments.entity.CommentEntity;
+import com.infinity.fashionity.members.entity.MemberEntity;
 import com.infinity.fashionity.posts.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.xml.stream.events.Comment;
 import java.util.List;
@@ -12,6 +15,14 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
     List<CommentEntity> findAllByPost(PostEntity post);
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
+
+    @Query("select c,count(cl),case when m is not null then true else false end as liked from CommentEntity c " +
+            "left join PostEntity p on c.post = p " +
+            "left join CommentLikeEntity cl on cl.comment = c " +
+            "left join MemberEntity m on cl.member = m and m = :member " +
+            "where p = :post " +
+            "group by c")
+    Page<Object[]> findAllByPostWithCommentsAndLikeCount(@Param("post") PostEntity post, @Param("member") MemberEntity member, Pageable pageable);
 
     Long countBySeq(long seq);
 }
