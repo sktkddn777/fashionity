50bd143eff84549d284974943197f71216e44db5
kimeunsol <kimeunsol95@naver.com>
2023-08-01 17:53:41 +0900

    [Feat] memberSeq로 게시글 ì조회 추가

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/posts/repository/PostRepository.java
+++ Back-End/fashionity/src/main/java/com/infinity/fashionity/posts/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.infinity.fashionity.posts.repository;
 
+import com.infinity.fashionity.members.entity.MemberEntity;
 import com.infinity.fashionity.posts.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,10 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.lang.reflect.Member;
+import java.util.List;
+
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
     Page<PostEntity> findAll(Pageable pageable);
+
+    List<PostEntity> findAllByMember(MemberEntity member);
 
 }
