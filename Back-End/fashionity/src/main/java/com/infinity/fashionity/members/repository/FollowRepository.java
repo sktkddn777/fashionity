e051c29b18e756961a70fb9646e55d0e98e7ebfb
한상우 <sktkddn777@naver.com>
2023-08-02 20:59:08 +0900

    [Hotfix] member.followEntity > follow.followEntity

--- Back-End/fashionity/src/main/java/com/infinity/fashionity/members/repository/FollowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.infinity.fashionity.members.repository;
-
-import com.infinity.fashionity.members.entity.FollowEntity;
-import com.infinity.fashionity.members.entity.FollowKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowRepository extends JpaRepository<FollowEntity, FollowKey> {
-}
