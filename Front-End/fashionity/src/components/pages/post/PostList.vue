7f7f211c904c212da460c28e30af6500d8b2e691
김현욱 <rlagusdnr120@gmail.com>
2023-08-03 16:31:55 +0900

    [Feat] Post 메인화면 api연동

--- Front-End/fashionity/src/components/pages/post/PostList.vue
+++ Front-End/fashionity/src/components/pages/post/PostList.vue
@@ -23,56 +23,60 @@
     <div class="row" style="height: 30px"></div>
 
     <div class="container">
-      <div class="row" style="justify-content: center">
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
+      <div v-if = "dataLoaded">
+        <div class="row" style="justify-content: center" v-for="(arr,index) in postRow" :key="index">
+          <div class="col" v-for="post in arr" :key="post.post_seq" >
+            <the-post :post="post"/>
+          </div>
         </div>
       </div>
-
-      <div class="row" style="height: 30px"></div>
-
-      <div class="row">
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
-        </div>
-        <div class="col">
-          <the-post></the-post>
-        </div>
+      <div v-else>
+        Loading....
       </div>
-
-      <div class="row" style="height: 40px"></div>
     </div>
   </div>
 </template>
 <script>
 import thePost from "@/components/shared/ThePost";
 import TheNavBarPost from "@/components/layout/TheNavBarPost.vue";
+import axios from 'axios';
 
 export default {
   data() {
     return {
       keyword: "",
+      page:0,
+      posts:[],
+      dataLoaded:false,
+      itemPerRow:4,
     };
   },
   components: {
     thePost,
     TheNavBarPost,
   },
+  computed:{
+    postRow(){
+      const itemsPerRow = 4;
+      const rows = [];
+      for (let i = 0; i < this.posts.length; i += itemsPerRow) {
+        rows.push(this.posts.slice(i, i + itemsPerRow));
+      }
+      return rows;
+    },
+  },
+  async mounted(){
+    axios({
+      url:"http://localhost/api/v1/posts",
+      method:"GET"
+    }).then((data)=>{
+      this.posts = data.data.posts;
+      this.dataLoaded = true;
+    })
+  },
+  methods:{
+    
+  }
 };
 </script>
 <style lang=""></style>
