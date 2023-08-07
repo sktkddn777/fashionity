6a63011a661c220412e3a7e1098012355ad28de5
pooiiiu4100@gmail.com <pooiiiu4100@gmail.com>
2023-08-07 13:21:29 +0900

    [Feat] WebRTC 관련 FE 추가

--- Front-End/fashionity/src/router/index.js
+++ Front-End/fashionity/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ProfilePage from "../components/pages/user/Profile.vue";
 import ProfileView from "../views/ProfileView.vue";
+import ConsultingPage from "../components/pages/consulting/Consulting-WebCam.vue";
+import ConsultingView from "../views/Consulting-WebCam-View.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +16,18 @@ const router = createRouter({
           path: "",
           name: "ProfilePage",
           component: ProfilePage,
+        },
+      ],
+    },
+    {
+      path: "/consulting",
+      name: "Consulting-WebCam-View",
+      component: ConsultingView,
+      children: [
+        {
+          path: "",
+          name: "ConsultingPage",
+          component: ConsultingPage,
         },
       ],
     },
