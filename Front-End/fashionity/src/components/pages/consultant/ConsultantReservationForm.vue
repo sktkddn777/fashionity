966655192f4583cd129cbff121ee8e39ca0ce811
정지원 <jiwon7164@naver.com>
2023-08-08 01:39:20 +0900

    [Style] ui form 수정

--- Front-End/fashionity/src/components/pages/consultant/ConsultantReservationForm.vue
+++ Front-End/fashionity/src/components/pages/consultant/ConsultantReservationForm.vue
@@ -1,8 +1,6 @@
 <template lang="">
   <div id="reservation-form" class="container-fluid scroll">
-    <div class="row justify-content-center detail-title">
-      상세 예약 정보 등록
-    </div>
+    <div class="row justify-content-center detail-title">상세 예약 정보 등록</div>
     <!-- 불러오기 버튼 -->
     <div class="row justify-content-end">
       <div class="col">
@@ -13,6 +11,51 @@
     <!-- 예약 정보 form  -->
     <div class="row">
       <form action="">
+        <!-- 나이, 성별 -->
+        <div class="row justify-content-center">
+          <div class="col">나이</div>
+          <div class="col">성별</div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col">
+            <input type="text" class="input-form" />
+          </div>
+          <div class="col">
+            <div class="pretty p-icon p-round">
+              <input type="radio" name="icon" />
+              <div class="state p-primary-o">
+                <i class="icon mdi mdi-check"></i>
+                <label>Dollar</label>
+              </div>
+            </div>
+
+            <div class="pretty p-icon p-round">
+              <input type="radio" name="icon" />
+              <div class="state p-success-o">
+                <i class="icon mdi mdi-check"></i>
+                <label>Euro</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row" style="height: 40px"></div>
+
+        <!-- 신장, 몸무게 -->
+        <div class="row justify-content-center">
+          <div class="col">신장</div>
+          <div class="col">몸무게</div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col">
+            <input type="text" class="input-form" />
+          </div>
+          <div class="col">
+            <input type="text" class="input-form" />
+          </div>
+        </div>
+
+        <!-- 평소 나의 스타일 등록 -->
         <div class="row">
           <multi-image-upload></multi-image-upload>
         </div>
@@ -143,5 +186,11 @@ export default {
 .scroll {
   -ms-overflow-style: none; /* 인터넷 익스플로러 */
   scrollbar-width: none; /* 파이어폭스 */
+}
+
+.input-form {
+  border: none;
+  border-bottom: 1px solid black; /* 밑줄 스타일 지정 */
+  padding: 5px;
 }
 </style>
