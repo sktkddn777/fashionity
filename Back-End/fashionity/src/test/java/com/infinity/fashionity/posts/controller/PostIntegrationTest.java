48e7b566a41df308b2c1aebf371123a03e9abf5c
김현욱 <rlagusdnr120@gmail.com>
2023-08-08 13:03:53 +0900

    [Test] Post List 조회 관련 테스트코드 추가

--- Back-End/fashionity/src/test/java/com/infinity/fashionity/posts/controller/PostIntegrationTest.java
+++ Back-End/fashionity/src/test/java/com/infinity/fashionity/posts/controller/PostIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.infinity.fashionity.posts.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infinity.fashionity.auth.dto.LoginDTO;
 import com.infinity.fashionity.comments.entity.CommentEntity;
 import com.infinity.fashionity.comments.entity.CommentLikeEntity;
 import com.infinity.fashionity.comments.repository.CommentLikeRepository;
@@ -16,6 +17,7 @@ import com.infinity.fashionity.members.repository.MemberRepository;
 import com.infinity.fashionity.posts.dto.PostListDTO;
 import com.infinity.fashionity.posts.entity.*;
 import com.infinity.fashionity.posts.repository.*;
+import com.infinity.fashionity.security.service.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -75,6 +77,9 @@ public class PostIntegrationTest {
     @Autowired
     private CommentLikeRepository commentLikeRepository;
 
+    @Autowired
+    private JwtProvider jwtProvider;
+
     private final String BASE_URL = "/api/v1/posts";
 
     private List<MemberEntity> members = new ArrayList<>();
@@ -91,6 +96,18 @@ public class PostIntegrationTest {
         return Math.abs(random.nextInt())%size;
     }
 
+    public LoginDTO.Response memberLogin(Long seq) throws Exception {
+        List<MemberRoleEntity> roles = new ArrayList<>();
+        roles.add(MemberRoleEntity.builder()
+                .member(MemberEntity.builder().seq(seq).build())
+                .memberRole(MemberRole.USER)
+                .build());
+        return LoginDTO.Response.builder()
+                .accessToken(jwtProvider.createAccessToken(seq, roles))
+                .build();
+    }
+
+
     @BeforeEach
     public void insertDummies(){
         //member setting
@@ -100,6 +117,7 @@ public class PostIntegrationTest {
                     .password("Abcdefg123!")
                     .nickname("tester".concat(Integer.toString(i)))
                     .sns(false)
+                    .profileUrl("profile"+i)
                     .email("tester".concat(Integer.toString(i)).concat("@gmail.com"))
                     .build();
             member.getMemberRoles().add(MemberRoleEntity.builder()
@@ -178,6 +196,7 @@ public class PostIntegrationTest {
                     commentLikes.put(comment.getSeq(),cles);
                 }
                 comments.put(post.getSeq(),commentEntities);
+//                post.getComments().addAll(commentEntities);
 
                 //post like setting
                 List<PostLikeEntity> postLikesEntities = new ArrayList<>();
@@ -239,7 +258,7 @@ public class PostIntegrationTest {
                         int l1 = postLikeRepository.findAllByPost(o1).size();
                         int l2 = postLikeRepository.findAllByPost(o2).size();
                         if(l1 == l2){
-                            return o2.getCreatedAt().compareTo(o1.getCreatedAt());
+                            return Long.compare(o2.getSeq(),o1.getSeq());
                         }
                         else return Integer.compare(l2,l1);
                     })
@@ -258,10 +277,65 @@ public class PostIntegrationTest {
             //actual
             PostListDTO.Response response = mapper.readValue(result.getResponse().getContentAsString(), PostListDTO.Response.class);
 
+            for(int i=0;i<response.getPosts().size();i++){
+                PostListDTO.Post post = response.getPosts().get(i);
+                PostListDTO.Post expect = expected.get(i);
+                assertThat(post.getPostSeq()).isEqualTo(expect.getPostSeq());
+                assertThat(post.getName()).isEqualTo(expect.getName());
+                assertThat(post.getLikeCount()).isEqualTo(expect.getLikeCount());
+                assertThat(post.getCommentCount()).isEqualTo(expect.getCommentCount());
+                assertThat(post.isLiked()).isEqualTo(expect.isLiked());
+                assertThat(post.getProfileImg()).isEqualTo(expect.getProfileImg());
+                assertThat(post.getImages().size()).isEqualTo(expect.getImages().size());
+            }
+        }
+
+        @Test
+        @DisplayName("success with default get with authorization")
+        public void successWithDefaultGetWithAuthorization() throws Exception {
+            String token = memberLogin(memberSeq).getAccessToken();
+            MvcResult result = sendRequest(token, dto)
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+
+
+            //expected values
+            List<PostListDTO.Post> expected = postLists.stream()
+                    .sorted((o1, o2) -> {
+                        int l1 = postLikeRepository.findAllByPost(o1).size();
+                        int l2 = postLikeRepository.findAllByPost(o2).size();
+                        if(l1 == l2){
+                            return Long.compare(o2.getSeq(),o1.getSeq());
+                        }
+                        else return Integer.compare(l2,l1);
+                    })
+                    .map(postEntity -> PostListDTO.Post.builder()
+                            .postSeq(postEntity.getSeq())
+                            .likeCount(postLikes.get(postEntity.getSeq()).size())
+                            .profileImg(postEntity.getMember().getProfileUrl())
+                            .commentCount(comments.get(postEntity.getSeq()).size())
+                            .liked(postLikes.get(postEntity.getSeq()).stream().filter(postLikeEntity -> postLikeEntity.getMember().getSeq() == memberSeq).findAny().isPresent())
+                            .content(postEntity.getContent())
+                            .images(images.get(postEntity.getSeq()).stream().map(e -> e.getUrl()).collect(Collectors.toList()))
+                            .name(postEntity.getMember().getNickname())
+                            .build())
+                    .collect(Collectors.toList());
+
+            //actual
+            PostListDTO.Response response = mapper.readValue(result.getResponse().getContentAsString(), PostListDTO.Response.class);
+
             //comment count 해결하기
             for(int i=0;i<response.getPosts().size();i++){
                 PostListDTO.Post post = response.getPosts().get(i);
-                assertThat(post).isSameAs(expected.get(i));
+                PostListDTO.Post expect = expected.get(i);
+                assertThat(post.getPostSeq()).isEqualTo(expect.getPostSeq());
+                assertThat(post.getName()).isEqualTo(expect.getName());
+                assertThat(post.getLikeCount()).isEqualTo(expect.getLikeCount());
+                assertThat(post.getCommentCount()).isEqualTo(expect.getCommentCount());
+                assertThat(post.isLiked()).isEqualTo(expect.isLiked());
+                assertThat(post.getProfileImg()).isEqualTo(expect.getProfileImg());
+                assertThat(post.getImages().size()).isEqualTo(expect.getImages().size());
             }
         }
     }
