4ad2e38bb1448d1211d55ca337f45fa85b190652
정지원 <jiwon7164@naver.com>
2023-08-08 17:12:47 +0900

    [Feat]

--- Front-End/fashionity/src/components/layout/TheHeader.vue
+++ Front-End/fashionity/src/components/layout/TheHeader.vue
@@ -18,10 +18,12 @@
                 <div></div>
                 <div class="col-6"></div>
                 <div class="col">
-                  <font-awesome-icon
-                    :icon="['fas', 'circle-plus']"
-                    style="color: #bdbdbd"
-                  />
+                  <router-link to="post/write" class="link">
+                    <font-awesome-icon
+                      :icon="['fas', 'circle-plus']"
+                      style="color: #bdbdbd"
+                    />
+                  </router-link>
                 </div>
                 <div class="col">
                   <font-awesome-icon
@@ -30,11 +32,6 @@
                   />
                 </div>
                 <div class="col">
-                  <!-- <font-awesome-icon
-                    :icon="['far', 'bell']"
-                    style="color: #bdbdbd"
-                    type="button"
-                  /> -->
                   <button
                     type="button"
                     data-bs-toggle="offcanvas"
