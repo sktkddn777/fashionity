5c81927a69298c1f5233e4d9af8a837c6954a1bb
김현욱 <rlagusdnr120@gmail.com>
2023-08-08 16:22:28 +0900

    [Feat] 좋아요 토글 및 무한스크롤 기능 적용

--- Front-End/fashionity/src/components/pages/post/PostList.vue
+++ Front-End/fashionity/src/components/pages/post/PostList.vue
@@ -48,6 +48,7 @@ export default {
       page: 0,
       posts: [],
       dataLoaded: false,
+      loadingNextPage:false,
       itemPerRow: 4,
     };
   },
@@ -66,15 +67,75 @@ export default {
     },
   },
   async mounted() {
+    // 스크롤 이벤트 리스너 추가
+    window.addEventListener("scroll", this.handleScroll);
+
+    let token = sessionStorage.getItem("token");
     axios({
-      url: "http://localhost/api/v1/posts",
+      url: `${process.env.VUE_APP_API_URL}/api/v1/posts`,
+      headers:{
+        "Authorization" : `Bearer ${token}`
+      },
       method: "GET",
     }).then((data) => {
       this.posts = data.data.posts;
+      console.log(this.posts);
       this.dataLoaded = true;
+    }).catch((exception)=>{
+      let data = (exception.response.data);
+      if(data.code === 'A004'){//유효기간이 다 된 토큰이면 일단 보여주셈
+        axios({
+          url:`${process.env.VUE_APP_API_URL}/api/v1/posts`,
+          method:'GET'
+        }).then((data)=>{
+          this.posts = data.data.posts;
+          this.dataLoaded = true;
+        })
+      }
     });
   },
-  methods: {},
+  methods: {
+    async loadNextPage(){
+      if(this.loadingNextPage) return;
+
+      this.loadingNextPage = true;
+      this.page++;
+
+      let token = sessionStorage.getItem("token");
+
+      axios({
+        url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page}`,
+        headers:{
+          "Authorization" : `Bearer ${token}`
+        },
+        method:"GET"
+      }).then((response)=>{
+        const newPosts = response.data.posts;
+        this.posts = [...this.posts,...newPosts];
+        this.loadingNextPage = false;
+      }).catch(exception=>{
+        this.loadingNextPage = false;
+        console.log(exception);
+      })
+    },
+    handleScroll() {
+      // 현재 스크롤 위치
+      const scrollY = window.scrollY;
+      // 뷰포트의 높이
+      const viewportHeight = window.innerHeight;
+      // 문서 전체의 높이
+      const fullHeight = document.documentElement.scrollHeight;
+
+      // 뷰포트 하단에 도달했을 때 (여기서 200은 여유값을 의미합니다.)
+      if (scrollY + viewportHeight >= fullHeight - 200) {
+        this.loadNextPage();
+      }
+    },
+  },
+  beforeUnmount() {
+    // 컴포넌트가 파괴되기 전에 스크롤 이벤트 리스너 제거
+    window.removeEventListener("scroll", this.handleScroll);
+  },
 };
 </script>
 <style lang=""></style>
