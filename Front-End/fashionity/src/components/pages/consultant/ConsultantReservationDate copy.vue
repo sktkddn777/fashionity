9a086e4593c31243573a76fd308c49b47226fbc7
정지원 <jiwon7164@naver.com>
2023-08-08 16:39:16 +0900

    [Feat] 이미지 croppper 띄우기

--- Front-End/fashionity/src/components/pages/consultant/ConsultantReservationDate copy.vue
+++ Front-End/fashionity/src/components/pages/consultant/ConsultantReservationDate copy.vue
@@ -1,25 +1,23 @@
 <template lang="">
   <div class="container-fluid" style="height: 65vh">
-    <!-- <v-date-picker width="400"></v-date-picker> -->
+    <v-date-picker width="400"></v-date-picker>
     <!-- <ConsultantCalenerTempVue></ConsultantCalenerTempVue> -->
-    <v-date-picker
-      v-model="date"
-      is-expanded
-      is-range
-      color="orange"
-    ></v-date-picker>
-
+    <!-- <v-date-picker
+        v-model="date"
+        is-expanded
+        is-range
+        color="orange"
+      ></v-date-picker> -->
+    <!-- 
     <div class="d-flex justify-center">
       <v-locale-provider locale="sv">
         <v-date-picker></v-date-picker>
-      </v-locale-provider>
-    </div>
+      </v-locale-provider> -->
+    <!-- </div> -->
   </div>
 </template>
 <script>
 // import ConsultantCalenerTempVue from "./ConsultantCalenerTemp.vue";
-
-import "v-calendar/style.css";
 
 export default {
   data() {
