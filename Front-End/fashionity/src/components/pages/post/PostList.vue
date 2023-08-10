ac07557dcedbddc1f078a9a01f8c147603d35e05
김현욱 <rlagusdnr120@gmail.com>
2023-08-10 13:46:56 +0900

    [Feat] PostList & Post 디자인 완료

--- Front-End/fashionity/src/components/pages/post/PostList.vue
+++ Front-End/fashionity/src/components/pages/post/PostList.vue
@@ -2,41 +2,54 @@
   <div class="container-fluid">
     <the-nav-bar-post></the-nav-bar-post>
 
-    <div class="row justify-content-space-around">
-      <div class="col-3">
+    <div class="row justify-content-space-around tools" ref="toolsContainer">
+      <div class="col-3 search">
         <input
           type="text"
           id="search"
           name="search"
           placeholder="검색어를 입력하세요"
-          style="border: 1px solid #bdbdbd"
         />
       </div>
       <div class="col"></div>
-      <div class="col-3">
-        <span class="sortBtn" @click="sortBy('popular')"
-        :class="{ 'highlighted': sorting === 'popular' }">인기순</span>
+      <div class="col-3 sort">
+        <span
+          class="sortBtn"
+          @click="sortBy('popular')"
+          :class="{ highlighted: sorting === 'popular' }"
+          >인기순</span
+        >
         <span> | </span>
-        <span class="sortBtn" @click="sortBy('latest')"
-        :class="{ 'highlighted': sorting === 'latest' }">최신순</span>
+        <span
+          class="sortBtn"
+          @click="sortBy('latest')"
+          :class="{ highlighted: sorting === 'latest' }"
+          >최신순</span
+        >
       </div>
     </div>
 
-    <div class="row" style="height: 30px"></div>
+    <!-- <div class="row" style="height: 30px"></div> -->
 
-    <div class="container">
-      <div v-if = "dataLoaded">
-        <div class="row" style="justify-content: center" v-for="(arr,index) in postRow" :key="index">
-          <div class="col" v-for="post in arr" :key="post.post_seq" >
-            <router-link :to="{path : `/post/${post.post_seq}`}" style="text-decoration: none; color: #424242"> 
-              <the-post :post="post"/>
-            </router-link>
+    <div class="container" ref="contentContainer">
+      <div v-if="dataLoaded">
+        <div
+          class="row"
+          style="justify-content: center"
+          v-for="(arr, index) in postRow"
+          :key="index"
+        >
+          <div
+            class="col"
+            v-for="post in arr"
+            :key="post.post_seq"
+            style="margin-bottom: 20px"
+          >
+            <the-post :post="post" />
           </div>
         </div>
       </div>
-      <div v-else>
-        Loading....
-      </div>
+      <div v-else>Loading....</div>
     </div>
   </div>
 </template>
@@ -51,9 +64,9 @@ export default {
       page: 0,
       posts: [],
       dataLoaded: false,
-      loadingNextPage:false,
+      loadingNextPage: false,
       itemPerRow: 4,
-      sorting:'popular'
+      sorting: "popular",
     };
   },
   components: {
@@ -76,65 +89,80 @@ export default {
     let token = sessionStorage.getItem("token");
     // this.loadNextPage();
     axios({
-      url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page++}&s=${this.sorting}`,
-      headers: token === null ? null : {
-        "Authorization" : `Bearer ${token}`
-      },
+      url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page}&s=${this.sorting}`,
+      headers:
+        token === null
+          ? null
+          : {
+              Authorization: `Bearer ${token}`,
+            },
       method: "GET",
-    }).then((data) => {
-      this.posts = data.data.posts;
-      console.log(this.posts);
-      this.dataLoaded = true;
-    }).catch((exception)=>{
-      let data = (exception.response.data);
-      if(data.code === 'A004'){//유효기간이 다 된 토큰이면 일단 보여주셈
-        axios({
-          url:`${process.env.VUE_APP_API_URL}/api/v1/posts`,
-          method:'GET'
-        }).then((data)=>{
-          this.posts = data.data.posts;
-          this.dataLoaded = true;
-        })
-      }
-    });
+    })
+      .then((data) => {
+        this.posts = data.data.posts;
+        console.log(this.posts);
+        this.dataLoaded = true;
+        this.page++;
+      })
+      .catch((exception) => {
+        let data = exception.response;
+        if (data.status === 401) {
+          //유효기간이 다 된 토큰이면 일단 보여주셈
+          axios({
+            url: `${process.env.VUE_APP_API_URL}/api/v1/posts`,
+            method: "GET",
+          }).then((data) => {
+            this.posts = data.data.posts;
+            this.dataLoaded = true;
+          });
+        }
+      });
   },
   methods: {
-    async sortBy(order){
-      console.log("order = "+order);
-      this.sorting=order;
-      this.page=0;
+    async sortBy(order) {
+      this.sorting = order;
+      this.page = 0;
       this.posts = [];
       this.loadNextPage();
     },
-    async loadNextPage(){
-      if(this.loadingNextPage) return;
+    async loadNextPage() {
+      if (this.loadingNextPage) return;
 
       this.loadingNextPage = true;
 
       let token = sessionStorage.getItem("token");
 
       axios({
-        url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page++}&s=${this.sorting}`,
-        headers: token === null ? null : {
-        "Authorization" : `Bearer ${token}`
-      },
-        method:"GET"
-      }).then((response)=>{
-        const newPosts = response.data.posts;
-        this.posts = [...this.posts,...newPosts];
-        this.loadingNextPage = false;
-      }).catch((exception)=>{
-      let data = (exception.response.data);
-      if(data.code === 'A004'){//유효기간이 다 된 토큰이면 일단 보여주셈
-        axios({
-          url:`${process.env.VUE_APP_API_URL}/api/v1/posts`,
-          method:'GET'
-        }).then((data)=>{
-          this.posts = data.data.posts;
-          this.dataLoaded = true;
+        url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page}&s=${this.sorting}`,
+        headers:
+          token === null
+            ? null
+            : {
+                Authorization: `Bearer ${token}`,
+              },
+        method: "GET",
+      })
+        .then((response) => {
+          const newPosts = response.data.posts;
+          this.posts = [...this.posts, ...newPosts];
+          this.loadingNextPage = false;
+          this.page++;
         })
-      }
-    });
+        .catch((exception) => {
+          let data = exception.response;
+          if (data.status === 401) {
+            //유효기간이 다 된 토큰이면 일단 보여주셈
+            axios({
+              url: `${process.env.VUE_APP_API_URL}/api/v1/posts?page=${this.page}&s=${this.sorting}`,
+              method: "GET",
+            }).then((response) => {
+              const newPosts = response.data.posts;
+              this.posts = [...this.posts, ...newPosts];
+              this.loadingNextPage = false;
+              this.page++;
+            });
+          }
+        });
     },
     handleScroll() {
       // 현재 스크롤 위치
@@ -157,11 +185,35 @@ export default {
 };
 </script>
 <style>
-.sortBtn{
-  color:#BDBDBD;
+.tools {
+  margin-bottom: 30px;
+}
+.tools,
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+.tools,
+.container-fluid {
+  flex-wrap: wrap;
+}
+#search {
+  all: unset;
+  border: 1px solid rgba(189, 189, 189, 0.8);
+  color: #424242;
+  font-size: 12px;
+  width: 200px;
+  height: 30px;
+  border-radius: 20px;
+}
+.sort {
+  white-space: nowrap;
+}
+.sortBtn {
+  color: #bdbdbd;
 }
 .highlighted {
-  color:#424242;
+  color: #424242;
   font-weight: bold; /* 원하는 스타일로 변경 */
 }
 </style>
