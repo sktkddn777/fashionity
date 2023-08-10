55859345f24d3f11317b775c4a61f1ecd41f66cb
한상우 <sktkddn777@naver.com>
2023-08-10 17:31:21 +0900

    [Feat] FE Logout 구현

--- Front-End/fashionity/src/components/pages/oauth2/Oauth2Redirect.vue
+++ Front-End/fashionity/src/components/pages/oauth2/Oauth2Redirect.vue
@@ -6,18 +6,30 @@
 import router from "@/router";
 import jwt_decode from "jwt-decode";
 import store from "@/store";
+import axios from "axios";
 
 export default {
   created() {
     const token = this.$route.query.accessToken;
     let user = jwt_decode(token);
 
-    const data = {
-      accessToken: token,
-      memberSeq: user.id,
-    };
-    store.commit("memberStore/LOGIN", data);
-    router.push({ name: "main" });
+    axios({
+      url: `${process.env.VUE_APP_API_URL}/api/v1/members`,
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then(({ data }) => {
+        const oauthUserInfo = {
+          accessToken: token,
+          memberSeq: user.id,
+          profileUri: data.profileUrl,
+        };
+        store.commit("memberStore/LOGIN", oauthUserInfo);
+        store.commit("memberStore/SET_IS_VALID_TOKEN", true);
+        router.push({ name: "main" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
--- Front-End/fashionity/src/router/index.js
+++ Front-End/fashionity/src/router/index.js
@@ -18,27 +18,37 @@ import PostList from "../components/pages/post/PostList.vue";
 import PostDetail from "../components/pages/post/PostDetail.vue";
 import PostWrite from "../components/pages/post/PostWrite.vue";
 import PostModify from "../components/pages/post/PostModify.vue";
-// import store from "@/store";
+import store from "@/store";
 
-// const onlyAuthUser = async (to, from, next) => {
-//   const checkUserInfo = store.getters["memberStore/checkUserInfo"];
-//   const checkToken = store.getters["memberStore/checkToken"];
-//   let token = sessionStorage.getItem("access-token");
-//   console.log("로그인 처리 전", checkUserInfo,  token);
+const onlyAuthUser = async () => {
+  const checkLoginUser = store.getters["memberStore/checkLoginUser"];
+  const checkToken = store.getters["memberStore/checkToken"];
 
-//   if (checkUserInfo != null && token) {
-//     console.log("토큰 유효성 체크하러 가자!!!!");
-//     await store.dispatch("memberStore/getUserInfo", token);
-//   }
-//   if (!checkToken || checkUserInfo === null) {
-//     alert("로그인이 필요한 페이지입니다..");
-//     // next({ name: "login" });
-//     router.push({ name: "userlogin" });
-//   } else {
-//     console.log("로그인 했다!!!!!!!!!!!!!.");
-//     next();
-//   }
-// };
+  if (checkLoginUser != null) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await store.dispatch("memberStore/getUserInfoAction");
+  }
+  if (!checkToken || checkLoginUser === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    router.push({ name: "UserLogin" });
+  } else {
+    console.log("로그인 한 유저네용");
+  }
+};
+
+const optionalAuthUser = async () => {
+  const checkLoginUser = store.getters["memberStore/checkLoginUser"];
+  const checkToken = store.getters["memberStore/checkToken"];
+
+  if (checkLoginUser != null) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await store.dispatch("memberStore/getUserInfoAction");
+  }
+
+  if (!checkToken || checkLoginUser === null) {
+    await store.dispatch("memberStore/logoutAction");
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(),
@@ -56,6 +66,7 @@ const router = createRouter({
         {
           path: "",
           name: "ProfilePage",
+          beforeEnter: onlyAuthUser,
           component: ProfilePage,
         },
         {
@@ -109,6 +120,7 @@ const router = createRouter({
         {
           path: "",
           name: "postList",
+          beforeEnter: optionalAuthUser,
           component: PostList,
         },
         {
