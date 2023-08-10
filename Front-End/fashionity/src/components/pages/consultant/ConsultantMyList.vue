6fdb2eb2a4ea954263642c951b42975167ce1279
정지원 <jiwon7164@naver.com>
2023-08-10 12:49:36 +0900

    [Style] 나의 예약 list ui 수정 완료

--- Front-End/fashionity/src/components/pages/consultant/ConsultantMyList.vue
+++ Front-End/fashionity/src/components/pages/consultant/ConsultantMyList.vue
@@ -1,10 +1,11 @@
 <template lang="">
   <div class="container">
-    <div class="row justify-content-center">
+    <!-- 이용 예정 예약 -->
+    <div class="row justify-content-center block">
       <div class="col-4" style="font-weight: bold">이용 예정</div>
       <div class="col"></div>
     </div>
-    <div class="row align-items-center">
+    <div class="row align-items-center block">
       <div class="col-4">
         <div class="row justify-content-end">
           <img class="profile" src="../../../assets/img/hyeonwook.jpg" alt="" />
@@ -21,12 +22,55 @@
         <button class="consultant-mylist-cancel">예약취소</button>
       </div>
     </div>
+
+    <div class="row" style="height: 8vh"></div>
+
+    <!-- 지난 예약 -->
+    <div class="row justify-content-center block">
+      <div class="col-4" style="font-weight: bold">지난 예약 목록</div>
+      <div class="col"></div>
+    </div>
+    <div class="row align-items-center block">
+      <div class="col-4">
+        <div class="row justify-content-end">
+          <img class="profile" src="../../../assets/img/panda.png" alt="" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="row" style="font-size: 15px">
+          예약 일시 : 2023년 07월 17시 15시
+        </div>
+        <div class="row" style="font-size: 15px">담당 컨설턴트 : 푸바오</div>
+      </div>
+      <div class="col">
+        <button class="consultant-mylist-write-review">후기 작성</button>
+      </div>
+    </div>
+    <div class="row align-items-center block">
+      <div class="col-4">
+        <div class="row justify-content-end">
+          <img class="profile" src="../../../assets/img/panda.png" alt="" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="row" style="font-size: 15px">
+          예약 일시 : 2023년 07월 17시 15시
+        </div>
+        <div class="row" style="font-size: 15px">담당 컨설턴트 : 푸바오</div>
+      </div>
+      <div class="col">
+        <button class="consultant-mylist-modify-review">후기 수정</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
+.block {
+  margin-bottom: 15px;
+}
 .consultant-mylist-enter {
   background-color: red;
   color: white;
@@ -34,6 +78,22 @@ export default {};
   padding: 10px;
 }
 .consultant-mylist-cancel {
+  background-color: gray;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  margin-left: 10px;
+}
+
+.consultant-mylist-write-review {
+  background-color: blue;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  margin-left: 10px;
+}
+
+.consultant-mylist-modify-review {
   background-color: gray;
   color: white;
   border-radius: 10px;
