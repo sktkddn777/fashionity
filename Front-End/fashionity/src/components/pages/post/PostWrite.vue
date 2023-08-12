e6cf5814cf28025e9d231c635414aaeb29656c6c
kimeunsol <kimeunsol95@naver.com>
2023-08-12 17:48:38 +0900

    [Feat] 해시태그 미리보기

--- Front-End/fashionity/src/components/pages/post/PostWrite.vue
+++ Front-End/fashionity/src/components/pages/post/PostWrite.vue
@@ -7,31 +7,45 @@
         <div class="post-detail-header">
           <div class="post-detail-header-img">
             <img
-              src="../../../assets/img/hyeonwook.jpg"
-              alt=""
+              :src="myProfileImg || '../img/unknown.e083a226.png'"
+              alt="profileImg"
               class="profile"
             />
           </div>
           <div class="post-detail-header-info">
             <div class="post-detail-header-info-nickname fw-bold">
-              유저 닉네임
+              {{ this.myNickname }}
             </div>
           </div>
         </div>
         <!--이미지 업로드-->
-        <multi-image-upload></multi-image-upload>
+        <multi-image-upload @uploadImg="uploadImg"></multi-image-upload>
         <br />
-        <textarea
-          class="form-control"
-          rows="1"
-          placeholder="해시태그를 입력해주세요."
-        ></textarea>
+        <div style="text-align: left">
+          <textarea
+            class="form-control"
+            rows="1"
+            placeholder="해시태그를 입력해주세요."
+            v-model="tagInput"
+            @keyup.enter="addTag"
+          ></textarea>
+          <span
+            v-for="(tag, index) in tagList"
+            :key="index"
+            class="tag"
+            style="color: skyblue"
+          >
+            #{{ tag }}&nbsp;
+          </span>
+        </div>
         <br />
         <textarea
           class="form-control"
           rows="3"
           placeholder="내용을 입력해주세요."
+          v-model="contentInput"
         ></textarea>
+
         <div class="post-write-button">
           <button
             type="button"
@@ -40,7 +54,12 @@
           >
             <span style="font-size: smaller">&nbsp;취소&nbsp;</span>
           </button>
-          <button type="button" class="btn btn-dark" style="min-width: 70px">
+          <button
+            type="button"
+            class="btn btn-dark"
+            style="min-width: 70px"
+            @click="submitPost"
+          >
             <span style="font-size: smaller">&nbsp;등록&nbsp;</span>
           </button>
         </div>
@@ -50,13 +69,57 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 import MultiImageUpload from "../shared/MultiImageUpload.vue";
 export default {
   data() {
-    return {};
+    return {
+      myNickname: this.$store.getters["memberStore/checkLoginUser"].nickname,
+      myProfileImg:
+        this.$store.getters["memberStore/checkLoginUser"].profileImage,
+      tagInput: "",
+      tagList: [],
+      contentInput: "",
+      images: [],
+    };
   },
   components: {
     MultiImageUpload,
+  },
+  methods: {
+    addTag() {
+      if (this.tagInput) {
+        this.tagList.push(this.tagInput);
+        this.tagInput = "";
+      }
+    },
+    submitPost() {
+      const postData = {
+        images: [],
+        content: this.contentInput,
+        hashtags: this.tagList,
+      };
+      this.callPostSaveAPI(postData);
+    },
+    callPostSaveAPI(postData) {
+      let token = sessionStorage.getItem("token");
+      axios({
+        url: `${process.env.VUE_APP_API_URL}/api/v1/posts`,
+        headers:
+          token === null
+            ? null
+            : {
+                Authorization: `Bearer ${token}`,
+              },
+        method: "POST",
+        data: postData,
+      }).then((data) => {
+        console.log(data.data.postSeq);
+      });
+    },
+    uploadImg(images) {
+      this.images = images;
+    },
   },
 };
 </script>
