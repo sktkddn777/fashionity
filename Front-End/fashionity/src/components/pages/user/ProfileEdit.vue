d3d01f164b9811e4d304d7f984ba4a8a2081b6c7
배이경 <yikyung52@naver.com>
2023-08-14 18:30:29 +0900

    [Feat] Profile Edit

--- Front-End/fashionity/src/components/pages/user/ProfileEdit.vue
+++ Front-End/fashionity/src/components/pages/user/ProfileEdit.vue
@@ -22,29 +22,72 @@
           <button class="inactive-button" style="margin-right: 0.3rem">
             사진 삭제
           </button>
-          <button class="change-password-button" style="margin-right: 0.3rem">
+          <!-- <button class="change-password-button" style="margin-right: 0.3rem">
             비밀번호 변경
-          </button>
+          </button> -->
+        </div>
+      </div>
+    </div>
+    <div class="block" style="height: 3rem"></div>
+    <div class="container" style="flex-direction: column">
+      <div class="edit-info row" style="justify-content: center" align="left">
+        <div class="row">
+          <form v-on:submit.prevent="editProfile">
+            <h5><b>아이디</b></h5>
+            <div>{{ id }}</div>
+            <hr />
+            <h5><b>이메일</b></h5>
+            <div>{{ email }}</div>
+            <hr />
+
+            <div>
+              <h5>
+                <label for="nickname"><b>닉네임</b></label>
+              </h5>
+              <input id="nickname" type="text" v-model="nickname" />
+              <hr />
+            </div>
+            <div>
+              <h5>
+                <label for="profileIntro"><b>한줄 소개</b></label>
+              </h5>
+              <input
+                id="profileIntro"
+                type="text"
+                v-model="profileIntro"
+                style="width: 500px"
+              />
+              <hr />
+            </div>
+          </form>
         </div>
       </div>
     </div>
 
-    <div class="edit-info" style="justify-content: center" align="left">
-      <div>아이디</div>
-      <div><input v-model="id" style="width: 300px" /></div>
-      <div>닉네임</div>
-      <div><input v-model="nickname" style="width: 300px" /></div>
-      <div>이메일</div>
-      <div><input v-model="email" style="width: 300px" /></div>
-      <div>한줄소개</div>
-      <div>
-        <textarea v-model="profileIntro" style="width: 300px"></textarea>
+    <div class="container-fluid">
+      <div class="row" style="width: 85vw">
+        <!-- <div class="p" style="background-color: red"></div> -->
+        <div class="p-2 justify-content-end" style="text-align: end">
+          <button
+            class="active-button"
+            style="margin-right: 0.3rem"
+            @click="editProfile"
+          >
+            수정하기
+          </button>
+          <button class="delete-button" @click="deleteProfile">탈퇴하기</button>
+        </div>
+        <div class="p-2 d-flex justify-content-start">
+          <!-- <button class="delete-button">탈퇴하기</button> -->
+        </div>
       </div>
-    </div>
 
-    <button>수정하기</button>
-    <button>탈퇴하기</button>
+      <!-- <div class="buttons col" style="align-items: flex-end" align="right">
+          
+        </div> -->
+    </div>
   </div>
+  <div class=" " style="height: 2rem"></div>
 </template>
 <script>
 import TheNavBarMypage from "@/components/layout/TheNavBarMypage.vue";
@@ -70,6 +113,7 @@ export default {
     this.getProfile();
   },
   methods: {
+    async submitProfile() {},
     getProfile() {
       axios({
         method: "get",
@@ -84,7 +128,35 @@ export default {
         console.log(data);
       });
     },
-    editProfile() {},
+    async editProfile() {
+      const updatedProfile = {
+        profileImage: this.profileUrl,
+        nickname: this.nickname,
+        profileIntro: this.profileIntro,
+      };
+      axios({
+        method: "post",
+        url: `${process.env.VUE_APP_API_URL}/api/v1/members/edit`,
+        data: updatedProfile,
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((data) => {
+          console.log(data);
+          console.log("Profile updated successfully!");
+        })
+        .catch((error) => {
+          console.log(updatedProfile);
+          console.log(error);
+        });
+    },
+    async deleteProfile() {
+      alert("삭제됩니다");
+      axios({
+        method: "delete",
+        url: `${process.env.VUE_APP_API_URL}/api/v1/members/delete`,
+        headers: { Authorization: `Bearer ${token}` },
+      }).then((data) => console.log(data.success));
+    },
   },
 };
 </script>
@@ -122,6 +194,14 @@ export default {
   flex-shrink: 0;
   border-radius: 10px;
   background: #cecece;
+  color: #ffffff;
+}
+.delete-button {
+  width: 100px;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #ed4141;
   color: #ffffff;
 }
 </style>
